fcb721d923c646fd48eb0b1717513b35cb0d2962
zjianru <864156480@qq.com>
2024-04-08 19:38:50 +0800

    2024-04-08 [01]

---
+++ src/main/java/leetcode/editor/cn/easy/validParentheses/[20]Valid Parentheses.java
@@ -0,0 +1,75 @@
+package leetcode.editor.cn.easy.validParentheses;//Given a string s containing just the characters '(', ')', '{', '}', '[' and ']
+//', determine if the input string is valid. 
+//
+// An input string is valid if: 
+//
+// 
+// Open brackets must be closed by the same type of brackets. 
+// Open brackets must be closed in the correct order. 
+// Every close bracket has a corresponding open bracket of the same type. 
+// 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: s = "()"
+//Output: true
+// 
+//
+// Example 2: 
+//
+// 
+//Input: s = "()[]{}"
+//Output: true
+// 
+//
+// Example 3: 
+//
+// 
+//Input: s = "(]"
+//Output: false
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= s.length <= 10⁴ 
+// s consists of parentheses only '()[]{}'. 
+// 
+//
+// Related Topics 栈 字符串 👍 4419 👎 0
+
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class ValidParentheses {
+    private static final Map<Character, Character> map = new HashMap<>() {{
+        put('{', '}');
+        put('[', ']');
+        put('(', ')');
+        put('?', '?');
+    }};
+
+    public boolean isValid(String s) {
+        if (!s.isEmpty() && !map.containsKey(s.charAt(0))) {
+            return false;
+        }
+        LinkedList<Character> stack = new LinkedList<>(){{
+            add('?');
+        }};
+        for (Character c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                stack.addLast(c);
+            } else if (map.get(stack.removeLast()) != c) {
+                return false;
+            }
+        }
+        return stack.size() == 1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
