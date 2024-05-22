9f9cf5993f11cce3b453d8477ced43a630c72b9d
zjianru <864156480@qq.com>
2024-05-22 15:12:40 +0800

    2024-05-21

---
+++ src/main/java/leetcode/editor/cn/easy/doc/[58]Length of Last Word.java
@@ -0,0 +1,49 @@
+package leetcode.editor.cn.easy.doc;//Given a string s consisting of words and spaces, return the length of the
+//last word in the string. 
+//
+// A word is a maximal substring consisting of non-space characters only. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: s = "Hello World"
+//Output: 5
+//Explanation: The last word is "World" with length 5.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: s = "   fly me   to   the moon  "
+//Output: 4
+//Explanation: The last word is "moon" with length 4.
+// 
+//
+// Example 3: 
+//
+// 
+//Input: s = "luffy is still joyboy"
+//Output: 6
+//Explanation: The last word is "joyboy" with length 6.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= s.length <= 10⁴ 
+// s consists of only English letters and spaces ' '. 
+// There will be at least one word in s. 
+// 
+//
+// Related Topics 字符串 👍 700 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int lengthOfLastWord(String s) {
+        return s.trim().length() - s.trim().lastIndexOf(" ") - 1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
