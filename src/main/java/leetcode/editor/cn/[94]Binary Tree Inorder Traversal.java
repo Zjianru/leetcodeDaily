91cacdc56b6ab85a19242795c16b4de466b663e3
zjianru <864156480@qq.com>
2024-05-22 16:00:48 +0800

    2024-05-22

---
+++ src/main/java/leetcode/editor/cn/[94]Binary Tree Inorder Traversal.java
@@ -0,0 +1,99 @@
+//Given the root of a binary tree, return the inorder traversal of its nodes' 
+//values. 
+//
+// 
+// Example 1: 
+// 
+// 
+//Input: root = [1,null,2,3]
+//Output: [1,3,2]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: root = []
+//Output: []
+// 
+//
+// Example 3: 
+//
+// 
+//Input: root = [1]
+//Output: [1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the tree is in the range [0, 100]. 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//Follow up: Recursive solution is trivial, could you do it iteratively?
+//
+// Related Topics 栈 树 深度优先搜索 二叉树 👍 2084 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+
+
+
+
+
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            res.add(cur.val);
+            cur = cur.right;
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
